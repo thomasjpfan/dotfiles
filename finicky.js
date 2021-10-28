eedeb01e7d098271b547613b0015d2c19ade9acf
Thomas J. Fan <thomasjpfan@gmail.com>
2021-10-27 21:35:35 -0400

    ENH Update flinchy

--- finicky.js
+++ finicky.js
@@ -9,5 +9,16 @@ module.exports = {
           opener.bundleId === "com.tinyspeck.slackmacgap",
         browser: "Google Chrome"
       },
+      {
+        match: /^https?:\/\/meet\.google\.com\/.*$/,
+        browser: "Google Chrome"
+      },
+      {
+        // Open links in Safari when the option key is pressed
+        // Valid keys are: shift, option, command, control, capsLock, and function.
+        // Please note that control usually opens a tooltip menu instead of visiting a link
+        match: () => finicky.getKeys().option,
+        browser: "Safari"
+      }
     ]
   }
